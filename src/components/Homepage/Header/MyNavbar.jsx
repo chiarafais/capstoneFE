cd907b84c052bd6cf308bda262c169e2782b0a1a
chiarafais <chiara0915@gmail.com>
2024-07-28 18:27:29 +0200

    add logout function

--- src/components/Homepage/Header/MyNavbar.jsx
+++ src/components/Homepage/Header/MyNavbar.jsx
@@ -1,14 +1,33 @@
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Dropdown, Form, Row } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { DO_LOGIN, getMyProfile } from "../../../redux/actions";
+import { DO_LOGIN, doLogout, getMyProfile, updateMyProfile } from "../../../redux/actions";
 import MyLoginModal from "./MyLoginModal";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 
 const MyNavbar = () => {
   const dispatch = useDispatch();
   const [modalLoginShow, setModalLoginShow] = useState(false);
+  const [updateProfile, setUpdateProfile] = useState(false);
+
   const token = useSelector((state) => state.login.results);
   const userInfo = useSelector((state) => state.profile.results);
+
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+
+  const handleProfileUpdate = () => {
+    setUpdateProfile(false);
+
+    const updateData = {
+      name: name ? name : userInfo.name,
+      surname: surname ? surname : userInfo.surname,
+      username: username ? username : userInfo.username,
+    };
+
+    dispatch(updateMyProfile(updateData));
+  };
 
   useEffect(() => {
     if (localStorage.getItem("tkn")) {
@@ -47,10 +66,83 @@ const MyNavbar = () => {
 
             {userInfo && (
               <div className="text-end ">
-                <Button className="button-username">
-                  {userInfo.username}
-                  <i className="bi bi-person-circle mx-1"></i>
-                </Button>
+                <Dropdown as={ButtonGroup} className="dropdown-profile-user" autoClose={false}>
+                  <Button className="button-username">
+                    {userInfo.username}
+                    <i className="bi bi-person-circle mx-2"></i>
+                  </Button>
+
+                  <Dropdown.Toggle split id="dropdown-split-basic" className="trigger-dropdown" />
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item>
+                      <span className="dropdown-profile-label">USERNAME: </span>
+                      {!updateProfile && <span>{userInfo.username}</span>}
+                      {updateProfile && (
+                        <span>
+                          <Form.Control
+                            className="my-3"
+                            type="text"
+                            value={username}
+                            placeholder={userInfo.username}
+                            onChange={(e) => setUsername(e.target.value)}
+                          />
+                        </span>
+                      )}
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                      <span className="dropdown-profile-label">NAME: </span>
+                      {!updateProfile && <span>{userInfo.name}</span>}
+                      {updateProfile && (
+                        <span>
+                          <Form.Control
+                            className="my-3"
+                            type="text"
+                            value={name}
+                            placeholder={userInfo.name}
+                            onChange={(e) => setName(e.target.value)}
+                          />
+                        </span>
+                      )}
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                      <span className="dropdown-profile-label">SURNAME: </span>
+                      {!updateProfile && <span>{userInfo.surname}</span>}
+                      {updateProfile && (
+                        <span>
+                          <Form.Control
+                            className="my-3"
+                            type="text"
+                            value={surname}
+                            placeholder={userInfo.surname}
+                            onChange={(e) => setSurname(e.target.value)}
+                          />
+                        </span>
+                      )}
+                    </Dropdown.Item>
+                    <Dropdown.Item>
+                      <span className="dropdown-profile-label">EMAIL:</span> {userInfo.email}
+                    </Dropdown.Item>
+                    <div className="text-end">
+                      {!updateProfile && (
+                        <div className="d-flex justify-content-around">
+                          <span onClick={() => dispatch(doLogout())}>
+                            <i className="bi bi-box-arrow-left text-danger icon-user-dropdown"></i>
+                          </span>
+
+                          <span onClick={() => setUpdateProfile(true)}>
+                            <i className="bi bi-pencil-square icon-user-dropdown"></i>
+                          </span>
+                        </div>
+                      )}
+                      {updateProfile && (
+                        <span onClick={() => handleProfileUpdate()}>
+                          <i className="bi bi-check-square-fill text-success icon-user-dropdown"></i>
+                        </span>
+                      )}
+                    </div>
+                  </Dropdown.Menu>
+                </Dropdown>
               </div>
             )}
           </Col>
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -51,12 +51,10 @@ export const doLogin = (email, password) => {
 export const getAllBeaches = () => {
   return async (dispatch) => {
     try {
-      const response = await fetch(url + "beach", {
+      const response = await fetch(url + "beach/allbeach", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MjIwOTY1NTQsImV4cCI6MTcyMjcwMTM1NCwic3ViIjoiMSJ9.Jw1QhbPvcPHptsx6RMlpx4qdpFKarc1hnjtbv14uIQk",
         },
       });
 
@@ -116,5 +114,38 @@ export const getRefreshDay = () => {
     } catch (err) {
       console.log("errror", err);
     }
+  };
+};
+
+export const updateMyProfile = (profile) => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(url + "users/me", {
+        method: "PUT",
+        body: JSON.stringify(profile),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("tkn"),
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        dispatch({ type: GET_USER_INFO, payload: data });
+      } else {
+        throw new Error("Errore nella fetch login");
+      }
+    } catch (err) {
+      localStorage.clear("tkn");
+      console.log("errror", err);
+    }
+  };
+};
+
+export const doLogout = () => {
+  return async (dispatch) => {
+    localStorage.removeItem("tkn");
+    dispatch({ type: DO_LOGIN, payload: "" });
+    dispatch({ type: GET_USER_INFO, payload: "" });
   };
 };
